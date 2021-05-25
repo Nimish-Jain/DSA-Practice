e0e3d6ec83eec966c60f2c2db7ef6cad70d70205
Nimish <nimishjain00@gmail.com>
2021-05-25 21:33:56 +0530

    Updated!

---
+++ Dynamic Programming/411. Knapsack(0-1).cpp
@@ -0,0 +1,39 @@
+//Code :: Nimish Jain
+#include<bits/stdc++.h>
+#define endl "\n"
+
+using namespace std;
+
+int dp[1001][1001];
+int answer(int W, int wt[], int val[], int n) {
+    if (n == 0 || W == 0)
+        return 0;
+    if (dp[n - 1][W] != -1)
+        return dp[n - 1][W];
+
+    if (wt[n - 1] <= W) {
+        return dp[n - 1][W] = max(val[n - 1] + answer(W - wt[n - 1], wt, val, n - 1), answer(W, wt, val, n - 1));
+    }
+
+    return dp[n - 1][W] = answer(W, wt, val, n - 1);
+
+}
+
+int knapSack(int W, int wt[], int val[], int n)
+{
+    // Your code here
+    memset(dp, -1, sizeof(dp));
+    return answer(W, wt, val, n);
+}
+
+int32_t main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+
+
+
+    return 0;
+}
