94278190cb8a67eb10bb12dd700896c1cfc456e4
Nimish <nimishjain00@gmail.com>
2021-06-14 08:51:20 +0530

    Updated

--- Graphs/376. BellmanFordAlgo.cpp
+++ Graphs/376. BellmanFordAlgo.cpp
@@ -11,10 +11,22 @@ int isNegativeWeightCycle(int n, vector<vector<int>>edges)
 	dist[0] = 0; // let 0 be the source vertex
 
 	// relax all the edges n - 1 times
-	for (int i = 1; i < n; i++)
-		for (int j = 0; j < m; j++)
-			if (dist[edges[j][0]] != INT_MAX && dist[edges[j][0]] + edges[j][2] <= dist[edges[j][1]])
-				dist[edges[j][1]] = dist[edges[j][0]] + edges[j][2];
+	// for (int i = 1; i < n; i++)
+	// 	for (int j = 0; j < m; j++)
+	// 		//dist[u] + wt < dist[v], then update
+	// 		if (dist[edges[j][0]] != INT_MAX && dist[edges[j][0]] + edges[j][2] <= dist[edges[j][1]])
+	// 			dist[edges[j][1]] = dist[edges[j][0]] + edges[j][2];
+
+	for (int i = 0; i < V - 1; i++) {
+		for (auto edge : edges) {
+			int u = edge[0];
+			int v = edge[1];
+			int wt = edge[2];
+
+			if (dist[u] + wt < dist[v])
+				dist[v] = dist[u] + wt;
+		}
+	}
 
 	// now check for the negative cycle
 	for (int i = 1; i < n; i++)
